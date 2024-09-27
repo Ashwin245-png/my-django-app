ed8ee8e3c1f7e23a2e9e4965cced670c75790283
Roshan <mmkarrie554@gmail.com>
2024-09-28 01:53:14 +0530

    done vercel setting statifiles url

--- Myapp/settings.py
+++ Myapp/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles_build', 'staticfiles')
 
 STATICFILES_DIR = {
      os.path.join(BASE_DIR, 'public/static')
