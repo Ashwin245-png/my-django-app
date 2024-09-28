b8abd6f828333b54d4f2b103dd03888c412ab0df
Roshan <mmkarrie554@gmail.com>
2024-09-28 22:34:26 +0530

    added procfile

--- Myapp/settings.py
+++ Myapp/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
 
+PORT = os.getenv('PORT', '8000')
+
 STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
